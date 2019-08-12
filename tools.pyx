9fc293ff6283a5cc46f8930ade30b2669b726e24
Evans <1648711820@qq.com>
2019-08-12 17:01:59 +0800

    优化部分代码

--- tools.pyx
+++ tools.pyx
@@ -1,3 +1,4 @@
+# cython: language_level=3
 import random
 import numpy as np
 # 计算平均rgb值
@@ -39,23 +40,19 @@ cpdef cal_avg_rgb(int width, int height, object pix):
 #                 k = key
 #         return __all_img[k]
 
-#获取最佳素材(按平均rgb),version 1.1
+#获取最佳素材(按平均rgb),version 1.2
 cpdef find_by_rgb(dict __all_img,sub_rgb):
-        #cdef double sub_r, sub_g, sub_b
-        a,b,c = sub_rgb.split("-")
-        # sub_r, sub_g, sub_b = float(a),float(b),float(c)
-        # cdef list vec1 = np.sum([sub_r, sub_g, sub_b])
-        cdef double vec1 = np.sum([float(a),float(b),float(c)])
+        sub_r, sub_g, sub_b = sub_rgb.split("-")
+        cdef double vec1 = np.sum([float(sub_r),float(sub_g),float(sub_b)]) 
         cdef list vec2 = []
+        cdef dict keymap = {}
         cdef list subtract = []
         cdef list keymapitems = []
-        cdef dict keymap = {}
         cdef double src_r, src_g, src_b
         cdef double rgbsum
-        cdef list first_three_items = []
-        cdef list final_keys = []
         cdef int  y,element
         cdef object z
+        cdef object final_val
         for key in __all_img.keys():
             d,e,f = key.split("-")
             src_r, src_g, src_b = float(d),float(e),float(f)
@@ -69,11 +66,8 @@ cpdef find_by_rgb(dict __all_img,sub_rgb):
         for y, element in enumerate(subtract): 
             keymap[ keymapitems[y][0] ] = element   # keymapitems[y][0],即keymap字典的key
         subtract.sort()
-        first_three_items = [subtract[0],subtract[1],subtract[2]] #找出最小的前三个差值
-        for z in keymap:
-            if keymap[z] in first_three_items:
-                final_keys.append(z)
-        final_key = random.choice(final_keys)
+        final_val = random.choice(subtract[:3])  #找出最小的前三个差值,即[subtract[0],subtract[1],subtract[2]]，幷随机取一个
+        final_key = [z for z in keymap if keymap[z] == final_val][0]
         return __all_img[final_key]
 
 
